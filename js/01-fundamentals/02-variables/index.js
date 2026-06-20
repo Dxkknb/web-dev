0556b534f926d0e91a7e7927a217d4b718b1963b
Dxkknb <konanknbernard@gmail.com>
2026-06-20 16:30:34 +0000

    Variables: Exercise 3

--- js/01-fundamentals/02-variables/index.js
+++ js/01-fundamentals/02-variables/index.js
@@ -42,3 +42,48 @@ let ourPlanet = "Earth";
 let currentUserName = "John Doe";
 
 console.log(ourPlanet, currentUserName);
+
+// Exercise 3: Constant in uppercase
+
+const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
+const DAYS_PER_YEAR = 365;
+const DAYS_PER_MONTH = 30;
+
+/**
+ * Converts a number of days into a formatted age (years, months, days).
+ * @param nbDays
+ * @returns {string}
+ */
+function daysToAge(nbDays) {
+	const nbYears = Math.floor(nbDays / DAYS_PER_YEAR);
+	const remainingAfterYears = nbDays % nbYears;
+
+	const nbMonths = Math.floor(remainingAfterYears / DAYS_PER_MONTH);
+	const remainingDays =remainingAfterYears % DAYS_PER_MONTH;
+
+	return `${nbYears} years, ${nbMonths} months and ${remainingDays} days.`;
+}
+
+/**
+ * Calculates age based on a date of birth.
+ *@param {string} birthDate - format "JJ.MM.AAAA"
+ * @param {string} separator
+ * @returns {string}
+ *
+ * Example: birthDate = '18.04.1982'
+ */
+function calculateAge(birthDate, separator = ".") {
+	const [day, month, year] = birthDate
+		.split(separator)
+		.map(Number);
+
+	const oldDate = new Date(year, month - 1, day) // Month indexed at 0
+	const currentDate = new Date()
+
+	const nbDays = Math.floor((currentDate - oldDate) / MILLISECONDS_PER_DAY);
+
+	return daysToAge(nbDays)
+}
+
+const birthday = "18.04.1982";
+console.log(calculateAge(birthday));

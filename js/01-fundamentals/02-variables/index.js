89f6954cdb9cb030e0782606ee4e119616c1f989
Dxkknb <konanknbernard@gmail.com>
2026-06-20 13:18:48 +0000

    Declaring variables using the 'let' keyword

---
+++ js/01-fundamentals/02-variables/index.js
@@ -0,0 +1,10 @@
+// Declare a variable
+
+let message = "I love JavaScript";
+console.log(message);
+
+let user = "John Doe";
+let age = 25;
+let job = 'Data Engineer';
+
+console.log(`I'm ${user}. I've ${age} years old and i'm ${job}.`);

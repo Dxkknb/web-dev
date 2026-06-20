75c2ab11d7c6fd76b4426334cc6edd598998975f
Dxkknb <konanknbernard@gmail.com>
2026-06-20 13:25:19 +0000

    Reassign a variable

--- js/01-fundamentals/02-variables/index.js
+++ js/01-fundamentals/02-variables/index.js
@@ -8,3 +8,7 @@ let age = 25;
 let job = 'Data Engineer';
 
 console.log(`I'm ${user}. I've ${age} years old and i'm ${job}.`);
+
+job = "JavaScript Developer";
+
+console.log(`I've got a new job. I'm now a ${job}.`);

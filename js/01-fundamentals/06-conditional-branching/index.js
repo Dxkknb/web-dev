33c6ff1d26beeb9627e3eb9185611a4b19a7683d
Dxkknb <konanknbernard@gmail.com>
2026-06-21 21:28:40 +0000

    Conditional branching: boolean conversion

--- js/01-fundamentals/06-conditional-branching/index.js
+++ js/01-fundamentals/06-conditional-branching/index.js
@@ -5,3 +5,18 @@ let year = prompt('In which year was ECMAScript-2015 specification published?', 
 if (Number(year) === 2015) {
 	alert("You're right!");
 }
+
+// Boolean conversion
+
+let age = prompt("Provide your age: ");
+let isAuthorized = Number(age) >= 18;
+
+if (isAuthorized) {
+	alert("You're authorized to visit this website!")
+}
+
+let falsyValues = 0 && "" && null && undefined && NaN && false;
+
+if (!falsyValues) {
+	console.log("Falsy values!");
+}

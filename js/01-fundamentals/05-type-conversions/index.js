e891e5999358c33f46f3e0e3d83864887e9a2bc5
Dxkknb <konanknbernard@gmail.com>
2026-06-21 19:51:36 +0000

    Type conversions: numeric conversions

--- js/01-fundamentals/05-type-conversions/index.js
+++ js/01-fundamentals/05-type-conversions/index.js
@@ -5,3 +5,16 @@ console.log(typeof value); // boolean
 
 value = String(value);
 console.log(typeof value); // string
+
+// Numeric Conversion
+
+let numeric_string = "120";
+console.log(typeof numeric_string); // string
+
+let numeric_number = Number(numeric_string);
+console.log(typeof numeric_number); // number
+
+console.log(Number(null)); // 0
+console.log(Number(undefined)); // NaN
+console.log(Number(true)); // 1
+console.log(Number(false)); // 0

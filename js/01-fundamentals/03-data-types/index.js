7661e66584fa14e44c4952a7fa466489eecbccf4
Dxkknb <konanknbernard@gmail.com>
2026-06-21 11:33:07 +0000

    Data types: Number

---
+++ js/01-fundamentals/03-data-types/index.js
@@ -0,0 +1,12 @@
+// Number : float and integer
+
+let numberOfProductsInStock = 10;
+let productPrice = 1.75;
+
+let totalProductsPrice = numberOfProductsInStock * productPrice;
+
+console.log(`Total products price: $${totalProductsPrice}`);
+
+console.log("Infinity:", Infinity);
+console.log("-Infinity:", -Infinity);
+console.log("NaN:", NaN);

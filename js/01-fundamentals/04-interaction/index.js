956bab5054a93de13810adf251d4e65d70230280
Dxkknb <konanknbernard@gmail.com>
2026-06-21 13:31:14 +0000

    Interaction: prompt function

--- js/01-fundamentals/04-interaction/index.js
+++ js/01-fundamentals/04-interaction/index.js
@@ -2,3 +2,25 @@
 
 let name = "John Doe";
 alert(`Hello, ${name}!`);
+
+// Prompt
+
+let age = prompt("Enter your age:", 0);
+
+
+if (!Number.isNaN(age)) {
+	alert("Provide a correct age.");
+}
+else if (age === null) {
+	alert("Provide your age please.");
+} else if (age.trim() === '') {
+	alert("Provide a valid age, not an empty value.")
+} else {
+	age = parseInt(age);
+
+	if (age < 0) {
+		alert("Your age must be a positive integer.");
+	} else {
+		alert(`You're ${age} years old.`);
+	}
+}

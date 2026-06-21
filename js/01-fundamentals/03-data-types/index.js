0cc0f9a3ecd7dc20614de62c9813d98d5fcb4ef8
Dxkknb <konanknbernard@gmail.com>
2026-06-21 11:53:32 +0000

    Data types: Boolean

--- js/01-fundamentals/03-data-types/index.js
+++ js/01-fundamentals/03-data-types/index.js
@@ -17,3 +17,25 @@ let name = "John Doe";
 let greeting = `Hello, ${name}!`;
 
 console.log(greeting);
+
+// Boolean: true or false
+
+let isOnline = true;
+let userName = "John Doe";
+let userAge = 19;
+
+if (isOnline && userAge >= 18) {
+	console.log(`${userName} is allowed to access to the website.`);
+} else {
+	console.log("You're not allowed to access to the website.");
+}
+
+let productsInStockQuantity = 8;
+let orderedProductsQuantity = 10;
+let canBeDelivered = productsInStockQuantity >= orderedProductsQuantity;
+
+if (canBeDelivered) {
+	console.log(`We will be delivering the ${orderedProductsQuantity} items of product you ordered shortly.`);
+} else {
+	console.log(`There are ${productsInStockQuantity} items of product left in stock.`)
+}

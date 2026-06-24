b0206b4ea9789cca9059430650e447908d7a0f47
Dxkknb <konanknbernard@gmail.com>
2026-06-24 18:01:23 +0000

    Loops: break statement

--- js/01-fundamentals/08-loops/index.js
+++ js/01-fundamentals/08-loops/index.js
@@ -13,3 +13,15 @@ let numbers = [1, 2, 3]
 for (let i = 0; i < numbers.length; i++) {
 	console.log(`${i} -> ${numbers[i]}`);
 }
+
+// 'break' statement
+
+let stopIteration = 5
+
+for (let i = 10; i > 0; i--) {
+	if (i === stopIteration) {
+		break
+	}
+
+	console.log(`value: ${i}`);
+}

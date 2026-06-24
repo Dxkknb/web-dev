7b141639712ad08fd47c24e87ed6904d647769b8
Dxkknb <konanknbernard@gmail.com>
2026-06-24 22:17:43 +0000

    Functions: local variables

--- js/01-fundamentals/09-functions/index.js
+++ js/01-fundamentals/09-functions/index.js
@@ -5,3 +5,13 @@ function showMessage() {
 }
 
 showMessage();
+
+// Local variables
+function greet() {
+    let greeting = "Hello mate!";
+    console.log(greeting);
+}
+
+greet(); // Hello mate!
+
+//console.log(greeting); // greeting is not defined

219ddcc280ac2813fe1c177b3e01dc236f1e4b47
Dxkknb <konanknbernard@gmail.com>
2026-06-24 17:58:46 +0000

    Loops: for loop

--- js/01-fundamentals/08-loops/index.js
+++ js/01-fundamentals/08-loops/index.js
@@ -5,3 +5,11 @@ while( i < 10) {
 	console.log("Bonjour " + i);
 	i++;
 }
+
+// 'for' Loop
+
+let numbers = [1, 2, 3]
+
+for (let i = 0; i < numbers.length; i++) {
+	console.log(`${i} -> ${numbers[i]}`);
+}

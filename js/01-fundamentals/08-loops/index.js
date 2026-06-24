bc6b99f14e624f63208b258296fe33f6aefd51db
Dxkknb <konanknbernard@gmail.com>
2026-06-24 20:13:44 +0000

    Loops: Exercises

--- js/01-fundamentals/08-loops/index.js
+++ js/01-fundamentals/08-loops/index.js
@@ -34,3 +34,66 @@ for (let i = 0; i <= 10; i++) {
 
 	console.log(i);
 }
+
+/*
+ Exercises
+ */
+
+// Ex 1: Output even numbers in the loop
+
+console.log("Even numbers: ");
+
+for (let i = 2; i <= 10; i++) {
+	if (i % 2 === 1) continue;
+
+	console.log(i);
+}
+
+// Ex 2: Repeat until the input is correct
+
+let input = null;
+let min_number = 100
+
+while (input < min_number) {
+	input = prompt(`Enter a number greater than ${min_number}`);
+
+	if(input === null || input.trim() === '' || isNaN(Number(input))) {
+		alert("Retry!");
+		continue;
+	}
+
+	if (Number(input) < min_number) {
+		alert(`Provide a number greater than ${min_number}`);
+		continue;
+	}
+
+	alert("You provide a valid number");
+
+}
+
+// Ex 3: Output prime numbers
+
+/**
+ * Check if a number is a prime number
+ * @param {number} number
+ * @returns {boolean}
+ */
+function isPrime(number) {
+	if (number < 2) return false;
+
+	if (number === 2 || number === 3 ) return true;
+
+	if (number % 2 === 0 || number % 3 === 0) return false;
+
+	const divider = Math.floor(Math.sqrt(number));
+
+	for (let i = 5; i <= divider; i += 6) {
+		if ( number % i === 0 || number % (i + 2) === 0) return false;
+	}
+
+	return true;
+}
+
+for (let i = 0; i < 20; i++) {
+	if (isPrime(i)) console.log(i);
+}

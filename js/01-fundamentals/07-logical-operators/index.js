34d16304085b01163e1d8d3b765673a0c3bc8c2c
Dxkknb <konanknbernard@gmail.com>
2026-06-24 15:17:49 +0000

    Logical operators: exercises

--- js/01-fundamentals/07-logical-operators/index.js
+++ js/01-fundamentals/07-logical-operators/index.js
@@ -33,3 +33,74 @@ console.log(!true); // false
 console.log(!0); // true
 console.log(!!"non-empty string"); // ttrue
 console.log(!!null); // false
+
+/*
+Exercises
+ */
+
+// Ex 1: What's the result of OR?
+
+alert(null || 2 || undefined); // 2
+
+// Ex 2: What's the result of OR'ed alerts?
+
+alert(alert(1) || 2 || alert(3)); // 1 then 2
+
+// Ex 3: What is the result of AND?
+
+alert(1 && null && 2); // null
+
+// Ex 4: What is the result of AND'ed alerts?
+
+alert(alert(1) && alert(2)); // 1 then undefined
+
+// Ex 5: The result of OR AND OR
+
+alert( null || 2 && 3 || 4 ); // 3
+
+// Ex 6: Check the range between
+let age = 13;
+
+if (age >= 14 && age <= 90) {
+	console.log("You're alive");
+}
+
+// Ex 7: Check the range outside
+
+if (age < 14 || age > 90) {
+	console.log("You're young or dead.");
+}
+
+if (!(age >= 14 && age <= 90)) {
+	console.log("V2: You're young or dead.");
+}
+
+// Ex 8: A question about 'if'
+if (-1 || 0) alert( 'first' ); // Yes
+if (-1 && 0) alert( 'second' ); // No
+if (null || -1 && 1) alert( 'third' ); // Yes
+
+// Ex 9: Check the login
+
+const username = prompt("Username: ");
+
+if (username === null) {
+	alert("Provide a username.");
+}
+
+if(username === "admin") {
+	const password = prompt("Provide the password");
+
+	if (password === null) {
+		alert("Canceled!");
+	}
+
+	if (password.toLowerCase() === "themaster") {
+		alert("Welcome.");
+	}else {
+		alert("Wrong password.")
+	}
+
+} else {
+	alert("I don't know you.");
+}

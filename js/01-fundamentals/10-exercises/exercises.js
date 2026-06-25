bb305d00789a48d83e0cfa1a3ad602192c6f3fd1
Dxkknb <konanknbernard@gmail.com>
2026-06-25 14:51:03 +0000

    Exercises: series of five exercises

---
+++ js/01-fundamentals/10-exercises/exercises.js
@@ -0,0 +1,93 @@
+// Exercise 1
+
+const isFloat = (n) => n % 1 !== 0;
+
+/**
+ * Returns the next integer (n+1) if n is an integer.
+ * If n is a decimal number, it is rounded down to the nearest integer before returning the next integer.
+ * If the parameter is not a valid number (NaN), 0 is returned.
+ * @param {number} num
+ * @returns {number}
+ */
+function getNextInteger(num) {
+	if (isNaN(num)) return 0;
+
+	if (Number.isInteger(num)) return num + 1;
+
+	if (isFloat(num)) return Math.floor(num) + 1;
+}
+
+console.log(getNextInteger("234e"));
+console.log(getNextInteger(12));
+console.log(getNextInteger(2.76));
+
+// Exercise 2:
+/**
+ * Convert hours to seconds
+ * @param {number} hours
+ * @returns {null | number}
+ */
+function convertHoursToSeconds(hours) {
+	if (hours < 0 || isNaN(hours)) return null;
+
+	return Math.floor(hours * 3600);
+}
+
+console.log(convertHoursToSeconds(2.9));
+console.log(convertHoursToSeconds(-10));
+
+// Exercise 3
+
+function getRemainderWithoutModulo(dividend, divisor) {
+	if (divisor === 0) return null;
+
+	if (divisor > dividend) {
+		let intermediate = dividend;
+		dividend = divisor;
+		divisor = intermediate;
+	}
+
+	return dividend - divisor * Math.floor(dividend / divisor);
+
+}
+
+console.log(getRemainderWithoutModulo(17, 20));
+
+// Exercise 4:
+/**
+ * Find the minimum between two numbers
+ * @param {number} a
+ * @param {number} b
+ * @returns {number | null}
+ */
+function findMinimumOfTwo(a, b) {
+	if (isNaN(a) || isNaN(b)) return null;
+
+	return a >= b ? b : a;
+}
+
+console.log(findMinimumOfTwo(12, 2));
+
+// Exercise 5:
+/**
+ * Check if a number is even or odd
+ * @param n
+ * @returns {boolean}: true the number is even, false the number is odd
+ */
+function checkParityWithStrictBitwise(n) {
+	if(isFloat(n)) return false;
+
+	if (n === 0) {
+		return true;
+	} else if(n === 1) {
+		return false;
+	}
+
+	let remainder = getRemainderWithoutModulo(n, 2);
+
+	return remainder === 0;
+}
+
+for(let i = 0; i < 20; i++) {
+	console.log(`${i} -> ${checkParityWithStrictBitwise(i)}`);
+}

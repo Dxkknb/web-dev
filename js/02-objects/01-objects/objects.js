92057ae1aa642f873d4824dcb87b05f754dd9c0e
Dxkknb <konanknbernard@gmail.com>
2026-06-29 13:58:36 +0000

    Objects: Access to object property

--- js/02-objects/01-objects/objects.js
+++ js/02-objects/01-objects/objects.js
@@ -5,7 +5,12 @@ console.log(user);
 
 user = {
 	name: "John Doe",
-	age: 30
+	age: 30,
+	"likes birds": true
 };
 console.log(user);
 console.log(`Name: ${user.name}\nAge: ${user.age}`);
+
+if (user["likes birds"]) {
+	console.log(`${user.name} likes birds.`);
+}

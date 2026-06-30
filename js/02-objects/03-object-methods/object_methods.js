a38265d57f4e236e48dbb8644745493f506fa78a
Dxkknb <konanknbernard@gmail.com>
2026-06-30 15:37:16 +0000

    Object methods: exercise

--- js/02-objects/03-object-methods/object_methods.js
+++ js/02-objects/03-object-methods/object_methods.js
@@ -26,3 +26,46 @@ const animal = {
 
 animal.jump();
 animal.run();
+
+// Exercises
+
+// TODO: Create a calculator
+
+function isNumber(value) {
+	return !isNaN(value) && typeof value === "number";
+}
+
+const calculator = {
+	read() {
+		/**
+		 * @private
+		 * @type {number}
+		 */
+		this.a = Number(prompt("a: "));
+
+		/**
+		 * @private
+		 * @type {number}
+		 */
+
+		this.b = Number(prompt("b: "));
+	},
+	sum() {
+		if (isNumber(this.a) && isNumber(this.b)) {
+			return  this.a + this.b;
+		}
+
+		return 0;
+	},
+	mul() {
+		if (isNumber(this.a) && isNumber(this.b)) {
+			return  this.a * this.b;
+		}
+
+		return 0;
+	}
+}
+
+calculator.read();
+console.log("Sum:", calculator.sum());
+console.log("Multiplication:",calculator.mul());

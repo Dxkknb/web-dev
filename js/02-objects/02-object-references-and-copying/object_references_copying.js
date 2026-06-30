e2cd23f7987fbf1a264bc2fe089018ce1c7f931a
Dxkknb <konanknbernard@gmail.com>
2026-06-30 10:46:03 +0000

    Object references: Objects are referenced by pointing to an object in memory.

--- js/02-objects/02-object-references-and-copying/object_references_copying.js
+++ js/02-objects/02-object-references-and-copying/object_references_copying.js
@@ -8,3 +8,14 @@ message = "Hello mate!";
 console.log("message:", message);
 console.log("phrase:", phrase);
 
+// TODO: Object references
+
+let user = {
+	name: "John Doe"
+};
+
+let admin = user;
+admin.name = "Jack Doe";
+
+console.log("User:", user); // User: {name: "Jack Doe"}
+console.log("Admin:", admin); // Admin: {name: "Jack Doe"}

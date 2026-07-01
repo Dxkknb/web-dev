b78ce69cdd08c80127ba7e8edb31dc56e5d4c196
Dxkknb <konanknbernard@gmail.com>
2026-07-01 13:52:45 +0000

    Numbers: more ways to write a number

---
+++ js/03-data-types/02-numbers/numbers.js
@@ -0,0 +1,14 @@
+// TODO: More ways to write a number
+
+let billion = 1000000000;
+billion = 1_000_000_000;
+billion = 1e9;
+
+console.log("Billion:", billion);
+
+// TODO: toString(base) method
+
+let num = 255;
+console.log(num.toString(2)); // 11111111
+console.log(num.toString(8)); // 377
+console.log(num.toString(16)); // ff

edc20af11f01367cd4edcf894768168d0bced379
Dxkknb <konanknbernard@gmail.com>
2026-07-01 16:14:51 +0000

    Numbers: rounding

--- js/03-data-types/02-numbers/numbers.js
+++ js/03-data-types/02-numbers/numbers.js
@@ -12,3 +12,16 @@ let num = 255;
 console.log(num.toString(2)); // 11111111
 console.log(num.toString(8)); // 377
 console.log(num.toString(16)); // ff
+
+// TODO: Rounding
+
+console.log(Math.floor(3.1)); // 3
+console.log(Math.floor(-1.1)); // -2
+
+console.log(Math.ceil(3.1)); // 4
+console.log(Math.ceil(-1.1)); // -1
+
+console.log(Math.round(3.1)); // 3
+console.log(Math.round(3.6)); // 4
+console.log(Math.round(3.5)); // 4
+console.log(Math.round(-3.5)) // -3

61c6b5ae5a1107fd965484d3ed7323a825e9b8b6
Dxkknb <konanknbernard@gmail.com>
2026-07-02 09:08:05 +0000

    Strings: special characters

--- js/03-data-types/03-strings/strings.js
+++ js/03-data-types/03-strings/strings.js
@@ -5,3 +5,16 @@ let name = "John Doe";
 let greeting = `${greet} ${name}`;
 
 console.log(greeting);
+
+// TODO: Special characters
+
+let guestList = "Guests:\n *John\n * Pete\n * Mary";
+
+console.log(guestList);
+
+const progLanguages = ["Python", "Java", "JavaScript", "Kotlin"];
+
+console.log("Programming languages:");
+for (const progLanguage of progLanguages) {
+	console.log(`\v\t- ${progLanguage}`);
+}

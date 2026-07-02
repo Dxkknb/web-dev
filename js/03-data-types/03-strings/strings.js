fc1d74fa3d08ba7fcd16ebc0cf92ae0a292790a1
Dxkknb <konanknbernard@gmail.com>
2026-07-02 09:28:37 +0000

    Strings: Accessing to characters

--- js/03-data-types/03-strings/strings.js
+++ js/03-data-types/03-strings/strings.js
@@ -18,3 +18,29 @@ console.log("Programming languages:");
 for (const progLanguage of progLanguages) {
 	console.log(`\v\t- ${progLanguage}`);
 }
+
+// TODO: Accessing characters
+
+const str = "Hello John";
+
+// The first character
+
+console.log(str[0]); // H
+console.log(str.at(0)); // H
+
+// The last character
+
+console.log(str[str.length - 1]); // n
+console.log(str.at(-1)); // n
+
+// All characters
+
+for (let i = -1; i >= -str.length; i--) {
+	console.log(`At index ${i}: ${str.at(i)}`);
+}
+
+// Iterate over the string
+
+for (const char of str) {
+	console.log(char);
+}

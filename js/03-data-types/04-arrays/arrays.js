4adfd4ca7dda9bf039240680d6fd5b32e7c865d1
Dxkknb <konanknbernard@gmail.com>
2026-07-04 18:03:08 +0000

    Arrays: Replace array items

--- js/03-data-types/04-arrays/arrays.js
+++ js/03-data-types/04-arrays/arrays.js
@@ -17,3 +17,12 @@ console.log("First item:", fruits[0]); // Apple
 console.log("Second item:", fruits[1]); // Orange
 console.log("Last item:", fruits[fruits.length - 1]); // Plum
 
+// TODO: Replace array items
+
+fruits[2] = "Pear"; // replace the third item
+fruits[3] = "Lemon"; // add an item at index 3
+
+if (fruits.length) {
+	console.log("Fruits:", fruits); // Fruits: [ "Apple", "Orange", "Pear", "Lemon" ]
+}
+

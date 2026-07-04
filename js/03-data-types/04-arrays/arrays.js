579ffd478bfc3ce631134f8d4bc76756261ddc1b
Dxkknb <konanknbernard@gmail.com>
2026-07-04 17:57:44 +0000

    Arrays: Accessing to array items

--- js/03-data-types/04-arrays/arrays.js
+++ js/03-data-types/04-arrays/arrays.js
@@ -5,3 +5,15 @@ let points = new Array([1.6, 2], [3.8, 7]);
 
 console.log("Arr:", arr);
 console.log("Points:", points);
+
+// TODO: Accessing to array items
+/**
+ * Array of fruits
+ * @type {string[]}
+ */
+const fruits = ["Apple", "Orange", "Plum"];
+
+console.log("First item:", fruits[0]); // Apple
+console.log("Second item:", fruits[1]); // Orange
+console.log("Last item:", fruits[fruits.length - 1]); // Plum
+

5fc8fd970778e26cc2f2728ef43d1368a2026aea
Dxkknb <konanknbernard@gmail.com>
2026-07-04 20:55:40 +0000

    Arrays (Exercises): Arrays operations

--- js/03-data-types/04-arrays/exercises.js
+++ js/03-data-types/04-arrays/exercises.js
@@ -7,3 +7,12 @@ let shoppingCart = fruits;
 shoppingCart.push("Banana");
 
 console.log(fruits.length); // Should be 4
+
+// TODO: Array operations
+
+const styles = ["Jazz", "Blues"];
+
+styles.push("Rock-n-Roll"); // Append to the end
+styles[Math.floor(styles.length/2)] = "Classics";
+console.log(styles.shift());
+styles.unshift(...["Rap", "Reggae"]);

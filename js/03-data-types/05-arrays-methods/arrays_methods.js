cd78c742c6922578f24bd3ba40740c0b41dd1c05
Dxkknb <konanknbernard@gmail.com>
2026-07-05 17:51:20 +0000

    Arrays methods: array.slice

--- js/03-data-types/05-arrays-methods/arrays_methods.js
+++ js/03-data-types/05-arrays-methods/arrays_methods.js
@@ -13,3 +13,34 @@ console.log("After removing a fruit:", fruits);
 // remove a fruit with array.splice
 fruits.splice(1, 1);
 console.log("After removing another fruit:", fruits);
+
+// TODO: Slice
+
+class Person {
+	/**
+	 * @param {string} name
+	 * @param {number} age
+	 */
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	display() {
+		return `Name:${this.name}\nAge:${this.age} years old.\n`;
+	}
+}
+
+const person1 = new Person("Konan", 30);
+const person2 = new Person("Koffi", 29);
+const person3 = new Person("N'guessan", 28);
+const person4 = new Person("Bernard", 27);
+
+const persons = [person1, person2, person3, person4];
+
+// the last two persons
+const lastTwoPersons = persons.slice(persons.length - 2);
+
+for (const person of lastTwoPersons) {
+	console.log(person.display());
+}

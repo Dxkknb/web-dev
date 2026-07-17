8c7c41215fca65638625341b9b8b2ee18c4b52df
Dxkknb <konanknbernard@gmail.com>
2026-07-17 15:01:22 +0000

    Iterable: create an iterable

---
+++ js/03-data-types/06-iterables/iterables.js
@@ -0,0 +1,24 @@
+/*
+Iterables
+ */
+
+// TODO Symbol.Iterator
+
+let range = {
+	from: 1,
+	to: 5,
+	[Symbol.iterator]() {
+		this.current = this.from;
+		return this;
+	},
+	next() {
+		if (this.current <= this.to) {
+			return {done: false, value: this.current++};
+		}
+		return {done: true};
+	}
+};
+
+for (let num of range) {
+	console.log(num);
+}

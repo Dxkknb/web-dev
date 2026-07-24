e15b8c69d9fbc8392af68f6e87e59c62a1938f55
Dxkknb <konanknbernard@gmail.com>
2026-07-24 16:30:56 +0000

    Classes: create a class and an instance of class

---
+++ js/04-classes/01-basic-syntax/basic_syntax.js
@@ -0,0 +1,51 @@
+// Class Basic Syntax
+
+/**
+ * Represents a single student.
+ */
+class Student {
+    /**
+     * Creates a student.
+     *
+     * @param {number|string} id - Unique identifier of the student.
+     * @param firstName - Firstname of the student.
+     * @param lastName - Lastname of the student.
+     * @param grade - Grade of the student.
+     */
+    constructor(id,firstName, lastName, grade) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
+
+    /**
+     * Returns the full name of the student.
+     *
+     * @return {string} The full name of the student.
+     */
+    fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    /**
+     * Displays student's information.
+     */
+    display() {
+        console.log(`
+Full name: ${this.fullName()}
+Grade: ${this.grade}
+        `)
+    }
+}
+
+// TODO: Create a student and display his information
+
+const student = new Student(
+    1,
+    "John",
+    "Doe",
+    "Tle C"
+);
+
+student.display();

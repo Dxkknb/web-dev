60ae51aff3a547168ad1e640da2b782b2b175454
Dxkknb <konanknbernard@gmail.com>
2026-07-25 11:11:49 +0000

    Classes: authentification class

--- js/04-classes/01-basic-syntax/basic_syntax.js
+++ js/04-classes/01-basic-syntax/basic_syntax.js
@@ -63,3 +63,86 @@ const student = new Student(
 );
 
 student.display();
+
+
+/*
+Exercise: Rewrite a class
+ */
+
+// TODO: User Authentification
+
+/*
+=====================================
+Class User
+=====================================
+ */
+
+/**
+ * Represents a single user.
+ */
+class User {
+    /**
+     * Creates a user.
+     *
+     * @param {number} id - Unique identifier of the user
+     * @param {string} username - Username of the user.
+     * @param {string} email - Email of the user.
+     * @param {string} password - Non hashed password of the user.
+     * @param {boolean} [loggedIn=false] - Whether a user is logged in or not.
+     */
+    constructor(id, username, email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = "123456789abc";
+        this.loggedIn = false;
+    }
+
+    // Methods
+    /**
+     * Log in the user.
+     */
+    login() {
+        this.loggedIn = true;
+    }
+
+    /**
+     * Log out the user.
+     */
+    logout() {
+        this.loggedIn = false;
+    }
+
+    /**
+     * Change the user's password.
+     *
+     * @param {string} newPassword - New password of the user.
+     */
+    changePassword(newPassword) {
+        this.password = newPassword;
+    }
+
+    displayProfile() {
+        if (this.loggedIn) {
+            console.log(`
+Username: ${this.username}
+Email: ${this.email}
+        `);
+        } else {
+            console.log(`You're logged out!`);
+        }
+    }
+}
+
+/*
+=====================================
+DEMO
+=====================================
+ */
+
+const user = new User(1, 'dkknb2', 'dkknb2@gmail.com');
+
+user.login();
+user.changePassword("12345");
+
+user.displayProfile();

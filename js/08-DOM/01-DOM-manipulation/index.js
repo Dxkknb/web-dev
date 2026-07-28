b6ea94b79ebaa1a614533ac9e117ecb48215b4b9
Dxkknb <konanknbernard@gmail.com>
2026-07-28 09:05:12 +0000

    DOM: create and remove eleÃment

--- js/08-DOM/01-DOM-manipulation/index.js
+++ js/08-DOM/01-DOM-manipulation/index.js
@@ -19,4 +19,41 @@ listItems.forEach((li) => {
 document.body.classList.add("bg-dark");
 setTimeout(() => {
     document.body.classList.remove("bg-dark");
-}, 5000);
+}, 2000);
+
+// TODO: Walk through the DOM
+/**
+ * @type {Element}
+ */
+const importantBox = document.querySelector('#box-important');
+console.log(importantBox);
+
+/**
+ * All divs in div.container node
+ * @type {NodeListOf<Element>}
+ */
+const containerBoxes = document.querySelectorAll('.container > div');
+console.log(containerBoxes);
+
+// DOM tree
+
+const ul = document.querySelector('.list');
+console.log(ul.nodeName, ul.innerText)
+
+ul.setAttribute('hidden', 'hidden');
+ul.removeAttribute('hidden');
+console.log(ul.getAttribute('class'));
+ul.classList.add('bold');
+
+// Style
+const title = document.querySelector('.title');
+title.style.color = "darkblue";
+
+console.log(getComputedStyle(title).color)
+
+// Create Element
+
+const newLi = document.createElement('li');
+newLi.innerHTML = "Bonjour les gens!";
+
+document.querySelector('ul.list').append(newLi);

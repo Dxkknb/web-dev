d62609a7fe31e8591ea69cbb42720f27cf589b99
Dxkknb <konanknbernard@gmail.com>
2026-08-24 11:51:27 +0000

    DOM Tree:  value means  or

--- js/08-DOM/01-walking-the-DOM/index.js
+++ js/08-DOM/01-walking-the-DOM/index.js
@@ -3,6 +3,15 @@ console.log(document);
 console.log(document.body);
 console.log(document.head);
 
+// Null value
 
+const div = document.querySelector(".div");
+
+if (div !== null) {
+    console.log("The node `div` exists.");
+    console.log(div);
+} else {
+    console.log("The node doesn't exists.");
+}
 
 

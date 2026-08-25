3190667aff86a72f8eb5337bc34d8df94657f2a8
Dxkknb <konanknbernard@gmail.com>
2026-08-25 16:54:42 +0000

    DOM Tree: Element children

--- js/08-DOM/01-walking-the-DOM/index.js
+++ js/08-DOM/01-walking-the-DOM/index.js
@@ -14,4 +14,27 @@ if (div !== null) {
     console.log("The node doesn't exists.");
 }
 
+// Children
 
+const listChildren = document.querySelector(".list").children;
+
+Array.from(listChildren).forEach((element) => {
+    console.log(element.innerText);
+})
+
+const unorderedList = document.querySelector(".list");
+
+if (unorderedList !== null && unorderedList.hasChildNodes()) {
+    Array.from(unorderedList.children).forEach((element) => {
+        element.addEventListener("click", handleClick)
+    })
+}
+
+function handleClick(e) {
+    e.preventDefault();
+    e.target.classList.toggle("highlight");
+}
+
+for(const element of document.body.childNodes) {
+    console.log(element);
+}
